154d713dae8cba42b8eb501e890f76260f22bfea
JessePeeters <jesse@pcss.nl>
2019-07-22 20:20:18 +0200

    Undo the console.log for testing

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-console.log('test')
-
 const app = express()
 const port = process.env.PORT || 3000
 
